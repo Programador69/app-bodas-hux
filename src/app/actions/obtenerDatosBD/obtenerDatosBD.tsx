15bedccc03c1fdca54d566fcb86f08f665fae8f5
Programador69 <84876878+Programador69@users.noreply.github.com>
2025-08-09 11:59:43 -0600

    Actualizar obtenerDatosBD.tsx

--- src/app/actions/obtenerDatosBD/obtenerDatosBD.tsx
+++ src/app/actions/obtenerDatosBD/obtenerDatosBD.tsx
@@ -11,7 +11,7 @@ export async function obtenerDatosBD()  {
             throw new Error("La conexión a la base de datos no está inicializada")
         }
 
-        const { rows } = await pool.sql`SELECT * FROM clientes`;
+        const { rows } = await pool.sql`SELECT * FROM clientes GROUP BY fecharegistro DESC`;
         await pool.end()
 
         return rows;
