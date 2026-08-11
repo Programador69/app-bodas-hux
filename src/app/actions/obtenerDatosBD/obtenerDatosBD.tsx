28361bf7a32a16905870d4b64b86db15fc601e3b
Programador69 <84876878+Programador69@users.noreply.github.com>
2026-08-10 21:37:04 -0600

    Update obtenerDatosBD.tsx
    Se limito a 50 resultados

--- src/app/actions/obtenerDatosBD/obtenerDatosBD.tsx
+++ src/app/actions/obtenerDatosBD/obtenerDatosBD.tsx
@@ -11,7 +11,7 @@ export async function obtenerDatosBD()  {
             throw new Error("La conexión a la base de datos no está inicializada")
         }
 
-        const { rows } = await pool.sql`SELECT * FROM clientes ORDER BY fecharegistro DESC`;
+        const { rows } = await pool.sql`SELECT * FROM clientes ORDER BY fecharegistro DESC LIMIT 50`;
         await pool.end()
 
         return rows;
